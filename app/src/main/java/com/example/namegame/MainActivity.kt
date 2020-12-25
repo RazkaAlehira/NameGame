a07319ae29452e1c65f36b520b0f0c48ec138bfe
Raz950 <Futurekid950@gmail.com>
2020-12-25 15:50:28 +0700

    1

--- app/src/main/java/com/example/namegame/MainActivity.kt
+++ app/src/main/java/com/example/namegame/MainActivity.kt
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.namegame.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -18,12 +19,22 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        val inflater = layoutInflater
+        binding = ActivityMainBinding.inflate(inflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         btn_main.setOnClickListener {
             d("Dzaky Sayang", "Hello ${edt_text.text}!")
             txt_message.text = "Hello ${edt_text.text}!!!"
+
+            if (edt_text.text.toString() == "Badboyhalo") {
+                img_badboyhalo.setImageResource(R.drawable.badboyhalo)
+            } else if (edt_text.text.toString() == "Dream") {
+                img_badboyhalo.setImageResource(R.drawable.fundywife)
+            } else {
+                Toast.makeText(this, "Fill The Blank!?", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
